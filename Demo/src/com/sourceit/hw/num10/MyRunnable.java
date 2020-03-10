a5b54f614f2ffce974f18aaf589b67f6e9a5c8a8
Dmitriy <metro200192@gmail.com>
2020-03-11 00:41:42 +0200

    hw_Thread_3

--- Demo/src/com/sourceit/hw/num10/MyRunnable.java
+++ Demo/src/com/sourceit/hw/num10/MyRunnable.java
@@ -3,70 +3,41 @@ package com.sourceit.hw.num10;
 import java.util.concurrent.TimeUnit;
 
 public class MyRunnable implements Runnable {
-	 private static final int TIME = 10;
-	 private static int ITER = 200;
-
+	private int val1 = 0;
+	private int val2 = 0;
+	private int res = 0;
 	public static void main(String[] args) throws InterruptedException {
-		MyRunnable myRun1 = new MyRunnable();
-		Thread thread1 = new Thread(myRun1);
+		MyRunnable myRun = new MyRunnable();
+		Thread thread1 = new Thread(myRun);
+		Thread thread2 = new Thread(myRun);
+		
 		thread1.start();
-		
-		MyRunnable myRun2 = new MyRunnable();
-		Thread thread2 = new Thread(myRun2);
 		thread2.start();
-		///////////////////////////////////////////////////////////
-		Thread t3 = new Thread(() -> {
-			
-			try {
-			while(true) {
-					System.out.println(Thread.currentThread().getName());
-					Thread.sleep(10);
-				} 
-			
-			}catch (InterruptedException e) {
-				System.out.println("Stopped");
-			}
-		});
-		t3.start();
-		TimeUnit.SECONDS.sleep(5);     //stop main thread
-		t3.interrupt();
-		///////////////////////////////////////////////////////////////
 		
+		TimeUnit.SECONDS.sleep(5);
+		
+		thread1.interrupt();
+		thread2.interrupt();
 	}
-	 
+	
 	@Override
 	public void run() {
-		Counter c = new Counter();
 		try {
-			while(!Thread.currentThread().isInterrupted() && (ITER >= 0)) { //true
-				System.out.println(Thread.currentThread().getName() + ":  " + c.calculate());
-				c.counter1();
-			    Thread.sleep(this.TIME);
-			    c.counter2();
-			    ITER--;
-			    //System.out.println(ITER);
-			}
-		} catch (InterruptedException e) {
-			System.out.println(Thread.currentThread().getName() + " is finish!");
-			e.printStackTrace();
+			//synchronized(this) {
+				while(true) {
+					res = val1 - val2;
+					System.out.println(res);
+					val1++;
+					//TimeUnit.MILLISECONDS.sleep(10);
+					Thread.sleep(10);
+					val2++;
+			     } 
+			//}
+		
+		}catch (InterruptedException e) {
+			System.out.println("Stopped");
 		}
 	}
 	
-	class Counter{
-		private int value1 = 0;
-		private int value2 = 0;
-		
-		public void counter1() {
-			this.value1++;
-		}
-		public void counter2() {
-			this.value2++;
-		}
-		public double calculate() {
-			return value1 - value2;
-		}
-		
-		
-	}
 
 }
